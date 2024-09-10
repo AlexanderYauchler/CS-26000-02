04baf4797c4d093e8a844ba4b1f3bc3eaf15d9b7
AlexanderYauchler <146694243+AlexanderYauchler@users.noreply.github.com>
2024-09-10 12:41:17 -0400

    Actually finished project 2

--- Project-2/src/main/java/org/example/Main.java
+++ Project-2/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Position3D> numbers = new ArrayList<>();
+
+        for (int i = 0; i <= 10; i++) {
+            numbers.add(new Position3D());
+        }
+
+        System.out.println("First Element: " + numbers.get(0).toString());
+        System.out.println("Last Element: " + numbers.get(numbers.size() - 1).toString());
+
+        numbers.remove(1);
+
+        boolean containsOrigin = numbers.contains(new Position3D(0, 0, 0));
+        if (!containsOrigin) {
+            numbers.add(1, new Position3D(0, 0, 0));
+        }
+
+        System.out.println("Size of Array: " + numbers.size());
+
+        Collections.sort(numbers);
+        System.out.println("Farthest point from origin: " + numbers.get(0).toString());
     }
 }
